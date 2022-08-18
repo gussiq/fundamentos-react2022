5695b7030b45fcad6b48b89008b82f810fec1a97
Gustavo Siqueira <gustavo.vha@hotmail.com>
2022-08-18 18:48:18 -0300

    feedback Project web terminado

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from './Avatar'
+
+import styles from './Comment.module.css'
+
+export function Comment({ content }) {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/gussiq.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gustavo Siqueira</strong>
+              <time title="11 de maio ás 08:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
